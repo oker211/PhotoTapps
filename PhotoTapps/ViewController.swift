ab2b3d7f74c8f8222d79f085aa323d15fe6aeb8b
Filipp Slabodetsky <oker211@gmail.com>
2020-07-08 15:59:22 +0300

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Филипп Слабодецкий on 08.07.2020.
+//  Copyright © 2020 Filipp Slabodetsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
